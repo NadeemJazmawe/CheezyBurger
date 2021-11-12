6e54588a8a36bbe674674cd48126300f6355dee8
nadeem jazmawe <nadeemJaz5@hotmail.com>
2021-11-12 21:23:18 +0200

    add run build script

--- client/src/component/socialMedia/socialMedia.js
+++ client/src/component/socialMedia/socialMedia.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import './socialMedia.css'
+import {Link} from 'react-router-dom'
 
-import packgroundPic from "../../images/socialmediaPackground.jpg"
+import { AiFillFacebook, AiOutlineInstagram } from 'react-icons/ai'
 
 export default function socialMedia() {
     return (
@@ -10,7 +11,17 @@ export default function socialMedia() {
             <div className="packground">
                 <div className="image">
                     <div className="message">
-                        gsfgsd
+                        חיפשו אותנו ב-
+                        <Link to={{ pathname: "https://www.facebook.com/profile.php?id=100020901374371" }} target="_blank">
+                            <AiFillFacebook color="#e9c147"/>
+                        </Link>
+                        <Link to={{pathname:"https://www.facebook.com/profile.php?id=100020901374371"}} target="_blank" >
+                            <AiOutlineInstagram color="#e9c147"/>
+                        </Link>
+                    </div>
+
+                    <div className="photos">
+                        
                     </div>
                 </div>
             </div>
